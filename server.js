7c41e30d735d2a86b08c4d62d939a8d336a885f7
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-07-12 01:06:43 +0400

    new

--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
 const path = require('path');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 if (!mongoURI) {
   console.error("Error: MongoDB connection string is not defined in the environment variables.");
@@ -31,9 +28,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-// Routes
 app.use('/api/data', dataRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
