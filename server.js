3f1cdacfd2a0c3d02b5722123e1fa49a4570c09b
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-07-12 00:27:41 +0400

    new

--- server.js
+++ server.js
@@ -1,44 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const dataRoutes = require('./routes/dataRoutes');
-// const path = require('path');
-// require('dotenv').config(); // Load environment variables from .env file
-
-// const app = express();
-
-// // Middleware
-// app.use(cors()); // Enable CORS for all routes
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Serve static files from the "uploads" directory
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// // MongoDB connection
-// const mongoURI = process.env.MONGO_URI;
-// if (!mongoURI) {
-//   console.error("Error: MongoDB connection string is not defined in the environment variables.");
-//   process.exit(1); // Exit the application with a failure code
-// }
-
-// mongoose
-//   .connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log("Error connecting to MongoDB:", err));
-
-// // Routes
-// app.use('/api/data', dataRoutes);
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -70,6 +29,12 @@ mongoose
 
 // Routes
 app.use('/api/data', dataRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
